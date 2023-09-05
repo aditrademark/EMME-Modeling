0731cf00bcfb1129aab75f8f278578fb358ae83e
aditrademark <aditrademark@gmail.com>
2023-09-05 11:27:47 -0700

    Upload

--- ASU/Code/osm2gmns_asu.py
+++ ASU/Code/osm2gmns_asu.py
@@ -4,42 +4,48 @@ import pandas as pd
 import grid2demand as gd
 
 # # Download OSM map, if applicable
-# # og.downloadOSMData(3444656, 'asu.osm')
+# og.downloadOSMData(3444656, 'asu.osm')
 
-# # Produce routable network along with POIs
-# net = og.getNetFromFile('asu.osm', 
-#                         network_types=('auto','bike','walk'), 
-#                         POI=True,
-#                         strict_mode=True, 
-#                         default_lanes=True, 
-#                         default_speed=True, 
-#                         default_capacity=True, 
-#                         start_node_id=1, 
-#                         start_link_id=1)
-# default_lanes_dict = {'motorway': 4, 'trunk': 3, 'primary': 3, 'secondary': 2, 'tertiary': 2,
-#                       'residential': 1, 'service': 1, 'cycleway':1, 'footway':1, 'track':1,
-#                       'unclassified': 1, 'connector': 2}
-# default_speed_dict = {'motorway': 120, 'trunk': 100, 'primary': 80, 'secondary': 60, 'tertiary': 40,
-#                       'residential': 30, 'service': 30, 'cycleway':5, 'footway':5, 'track':30,
-#                       'unclassified': 30, 'connector':120}
-# default_capacity_dict = {'motorway': 2300, 'trunk': 2200, 'primary': 1800, 'secondary': 1600, 'tertiary': 1200,
-#                       'residential': 1000, 'service': 800, 'cycleway':800, 'footway':800, 'track':800,
-#                       'unclassified': 800, 'connector':9999}
-# og.outputNetToCSV(net)
+# Produce routable network along with POIs
+net = og.getNetFromFile('asu.osm', 
+                        network_types=('auto','bike','walk'), 
+                        POI=True,
+                        strict_mode=True, 
+                        default_lanes=True, 
+                        default_speed=True, 
+                        default_capacity=True, 
+                        start_node_id=1, 
+                        start_link_id=1)
+default_lanes_dict = {'motorway': 4, 'trunk': 3, 'primary': 3, 'secondary': 2, 'tertiary': 2,
+                      'residential': 1, 'service': 1, 'cycleway':1, 'footway':1, 'track':1,
+                      'unclassified': 1, 'connector': 2}
+default_speed_dict = {'motorway': 120, 'trunk': 100, 'primary': 80, 'secondary': 60, 'tertiary': 40,
+                      'residential': 30, 'service': 30, 'cycleway':5, 'footway':5, 'track':30,
+                      'unclassified': 30, 'connector':120}
+default_capacity_dict = {'motorway': 2300, 'trunk': 2200, 'primary': 1800, 'secondary': 1600, 'tertiary': 1200,
+                      'residential': 1000, 'service': 800, 'cycleway':800, 'footway':800, 'track':800,
+                      'unclassified': 800, 'connector':9999}
+og.connectPOIWithNet(net)
+og.generateNodeActivityInfo(net)
+og.outputNetToCSV(net)
 
-# # Modify the allowed_users (modes) in the link file to better-fit EMME's requirement
-# df = pd.read_csv('link.csv')
-# word_to_letter = {
-#     'auto':'a',
-#     'walk':'w',
-#     'bike':'b'}
+# Modify the allowed_users (modes) in the link file to better-fit EMME's requirement
+df = pd.read_csv('link.csv')
+word_to_letter = {
+    'auto':'a',
+    'walk':'w',
+    'bike':'b'}
 
-# df['allowed_uses'] = df['allowed_uses'].str.replace('|'.join(word_to_letter.keys()), 
-#                                                     lambda x: word_to_letter[x.group()], regex=True)
-# df['allowed_uses'] = df['allowed_uses'].str.replace(';', '')
-# df.to_csv('link.csv', index=False)
+df['allowed_uses'] = df['allowed_uses'].str.replace('|'.join(word_to_letter.keys()), 
+                                                    lambda x: word_to_letter[x.group()], regex=True)
+df['allowed_uses'] = df['allowed_uses'].str.replace(';', '')
+df.to_csv('link.csv', index=False)
 
 # Produce cell grids
-net = gd.ReadNetworkFiles(input_folder='C:\\Users\\aditr\\OneDrive\\Desktop\\ASU\\Modeling\\ASU\\Code')
-zone = gd.PartitionGrid(number_of_x_blocks=5, number_of_y_blocks=5)
-
+net = gd.ReadNetworkFiles('')
+zone = gd.PartitionGrid(number_of_x_blocks=5, number_of_y_blocks=5, latitude=30)
+triprate = gd.GetPoiTripRate(trip_rate_folder='',trip_purpose=1)
+nodedemand = gd.GetNodeDemand()
+accessibility = gd.ProduceAccessMatrix(latitude=30, accessibility_folder='')
+demand = gd.RunGravityModel(trip_purpose=1, a=None, b=None, c=None)
+demand = gd.GenerateAgentBasedDemand()
---
+++ Arizona/Code/osm2gmns_arizona.py
@@ -0,0 +1,41 @@
+# Import modules
+import osm2gmns as og
+import pandas as pd
+import grid2demand as gd
+
+# # Download OSM map, if applicable
+# og.downloadOSMData(162018, 'arizona.osm')
+
+# Produce routable network along with POIs
+net = og.getNetFromFile('arizona.osm', 
+                        network_types=('auto'), 
+                        POI=True,
+                        POI_sampling_ratio=0.1,
+                        strict_mode=True, 
+                        default_lanes=True, 
+                        default_speed=True, 
+                        default_capacity=True, 
+                        start_node_id=1, 
+                        start_link_id=1)
+default_lanes_dict = {'motorway': 4, 'trunk': 3, 'primary': 3, 'secondary': 2, 'tertiary': 2,
+                      'residential': 1, 'service': 1, 'cycleway':1, 'footway':1, 'track':1,
+                      'unclassified': 1, 'connector': 2}
+default_speed_dict = {'motorway': 120, 'trunk': 100, 'primary': 80, 'secondary': 60, 'tertiary': 40,
+                      'residential': 30, 'service': 30, 'cycleway':5, 'footway':5, 'track':30,
+                      'unclassified': 30, 'connector':120}
+default_capacity_dict = {'motorway': 2300, 'trunk': 2200, 'primary': 1800, 'secondary': 1600, 'tertiary': 1200,
+                      'residential': 1000, 'service': 800, 'cycleway':800, 'footway':800, 'track':800,
+                      'unclassified': 800, 'connector':9999}
+og.outputNetToCSV(net)
+
+# # Modify the allowed_users (modes) in the link file to better-fit EMME's requirement
+# df = pd.read_csv('link.csv')
+# word_to_letter = {
+#     'auto':'a',
+#     'walk':'w',
+#     'bike':'b'}
+
+# df['allowed_uses'] = df['allowed_uses'].str.replace('|'.join(word_to_letter.keys()), 
+#                                                     lambda x: word_to_letter[x.group()], regex=True)
+# df['allowed_uses'] = df['allowed_uses'].str.replace(';', '')
+# df.to_csv('link.csv', index=False)
